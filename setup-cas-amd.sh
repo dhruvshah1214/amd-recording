b700a517a8a2aae95bba43805dbda11c3cf3e07d
Dhruv Shah <dhruv.shah@gmail.com>
2020-07-21 00:31:30 -0700

    Update setup-cas-amd.sh

--- setup-cas-amd.sh
+++ setup-cas-amd.sh
@@ -3,6 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir /home/ec2-user/amd
 mkdir /home/ec2-user/amd/recordings/
 
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p /home/ec2-user/miniconda3
 /home/ec2-user/miniconda3/bin/conda init
 yes "" | /home/ec2-user/miniconda3/bin/conda install -c conda-forge librosa
