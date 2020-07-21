c6f83908e86e08cd9f3f365f0027b2a4caff1de9
Dhruv Shah <dhruv.shah@gmail.com>
2020-07-21 00:21:30 -0700

    Update setup-cas-amd.sh

--- setup-cas-amd.sh
+++ setup-cas-amd.sh
@@ -9,5 +9,6 @@ yes "" | /home/ec2-user/miniconda3/bin/conda install -c conda-forge librosa
 yes "" | /home/ec2-user/miniconda3/bin/conda install numpy
 
 yes "" | /home/ec2-user/miniconda3/bin/pip install pyst2
+yes "" | /home/ec2-user/miniconda3/bin/pip install numba==0.48
 
 cp $DIR/* /home/ec2-user/amd/
