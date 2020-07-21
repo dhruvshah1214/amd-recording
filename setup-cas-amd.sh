c37010dc6af825e2342077dec4e611c76ac537ca
Dhruv Shah <dhruv.shah@gmail.com>
2020-07-21 00:05:23 -0700

    Update setup-cas-amd.sh

--- setup-cas-amd.sh
+++ setup-cas-amd.sh
@@ -1,15 +1,9 @@
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-    echo "Please run as root (sudo)."
-    exit
-fi
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 mkdir /home/ec2-user/amd
 mkdir /home/ec2-user/amd/recordings/
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b
+bash Miniconda3-latest-Linux-x86_64.sh -b -p /home/ec2-user/miniconda3
 /home/ec2-user/miniconda3/bin/conda init
 yes "" | /home/ec2-user/miniconda3/bin/conda install librosa
 yes "" | /home/ec2-user/miniconda3/bin/conda install numpy
